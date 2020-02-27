93ff0417fd3e349606b9f221695073455b8364b5
Natalia Souto <nataliasoutoo@gmail.com>
2020-02-27 15:33:03 -0300

    Iniciando Projeto Lista de Tarefas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyB-cVvKbNhkZk7rUl4vMR3hIJwjTUL5tlA",
+    authDomain: "task-list-angular-bc902.firebaseapp.com",
+    databaseURL: "https://task-list-angular-bc902.firebaseio.com",
+    projectId: "task-list-angular-bc902",
+    storageBucket: "task-list-angular-bc902.appspot.com",
+    messagingSenderId: "1015882836551",
+    appId: "1:1015882836551:web:d3ba12f7bccf7d2e0b5f62",
+    measurementId: "G-0R2PC2EVRW"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB-cVvKbNhkZk7rUl4vMR3hIJwjTUL5tlA",
+    authDomain: "task-list-angular-bc902.firebaseapp.com",
+    databaseURL: "https://task-list-angular-bc902.firebaseio.com",
+    projectId: "task-list-angular-bc902",
+    storageBucket: "task-list-angular-bc902.appspot.com",
+    messagingSenderId: "1015882836551",
+    appId: "1:1015882836551:web:d3ba12f7bccf7d2e0b5f62",
+    measurementId: "G-0R2PC2EVRW"
+  }
 };
 
 /*
