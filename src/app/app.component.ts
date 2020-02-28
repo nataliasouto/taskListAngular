19910634803cf57a5063f4184f0b1413967723bd
nataliasoutoo <nataliasoutoo@gmail.com>
2020-02-27 22:08:05 -0300

    criação do models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'task-list';
+export class AppComponent implements OnInit {
+  
+  tasks$: Observable<Task[]>
+
+  constructor(
+    private db: AngularFirestore
+  ){}
+  
+  ngOnInit(): void {
+    this.tasks$ = this.db.collection<Task>('/tasks').valueChanges()
+  }
+
 }
