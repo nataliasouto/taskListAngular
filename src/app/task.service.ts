51291ccce550bd863ce9c1f50da53d75cfecae09
nataliasoutoo <nataliasoutoo@gmail.com>
2020-02-29 09:52:15 -0300

    crud by id no firebase

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,6 +1,7 @@
 import { Task } from './models/task.model';
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class TaskService {
@@ -14,9 +15,33 @@ export class TaskService {
    }
 
   private setTasks(): void {
-    this.tasks = this.db.collection<Task>('/tasks');
-    
-    
+    this.tasks = this.db.collection<Task>('/tasks'); 
   }
   
+  create(task: Task): Promise<void> {
+    const uid = this.db.createId();
+    // trabalhando no /task
+    return this.tasks.doc<Task>(uid)
+      .set({
+        uid,
+        title: task.title,
+        done: false
+      });
+  }
+
+  update(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+      .update(task);
+    
+  }
+
+  delete(task: Task): Promise<void> {
+    return this.tasks.doc<Task>(task.uid)
+      .delete();
+  }
+
+  get(uid: string): Observable<Task> {
+    return this.tasks.doc<Task>(uid).valueChanges();
+  }
+
 }
